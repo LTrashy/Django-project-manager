0d14972cb34284b05b138c6cca957fea5bc3b78f
ander <asgonzaleztr@gmail.com>
2025-01-11 00:56:58 -0500

    company and home user

--- companies/views.py
+++ companies/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import UserProfile, Company
 from django.http import HttpResponse
@@ -11,49 +11,80 @@ from django.contrib.auth.decorators import login_required
 
 
 def home(request):
+    # print(request.user)
+    if request.user.is_authenticated:
+        userProfile = UserProfile.objects.get(user=request.user)
+        company = get_object_or_404(Company, id=userProfile.company.id)
+        return render(request, "home.html", {"company": company})
     return render(request, "home.html")
 
-def signup (request):
+
+def signup(request):
     if request.method == "GET":
-        return render(request, "signup.html", {"form":UserCreationForm})
+        return render(request, "signup.html", {"form": UserCreationForm})
     else:
         if request.POST["password1"] == request.POST["password2"]:
             # registrar usuario
             try:
-                user= User.objects.create_user(username=request.POST["username"], password=request.POST["password1"])
+                user = User.objects.create_user(
+                    username=request.POST["username"],
+                    password=request.POST["password1"],
+                )
                 user.save()
 
                 login(request, user)
                 return redirect("companies", userN=user)
             except Exception as e:
-                 return render(request, "signup.html", {"form":UserCreationForm,"error":"user already exist"})        
-        return render(request, "signup.html", {"form":UserCreationForm,"error":"Password do not match"})
+                return render(
+                    request,
+                    "signup.html",
+                    {"form": UserCreationForm, "error": "user already exist"},
+                )
+        return render(
+            request,
+            "signup.html",
+            {"form": UserCreationForm, "error": "Password do not match"},
+        )
+
 
 @login_required
-def companies (request, userN):
+def companies(request, userN):
     if request.method == "GET":
         companies = Company.objects.all()
-        return render(request, "companies.html", {"companies":companies})
+        return render(request, "companies.html", {"companies": companies})
     else:
-        user = User.objects.get(username=userN)           
+        user = User.objects.get(username=userN)
         company = Company.objects.get(nombre=request.POST["company"])
-        userProfile = UserProfile(user=user,company=company)
+        userProfile = UserProfile(user=user, company=company)
         userProfile.save()
         return redirect("/projects")
+
 
 @login_required
 def signout(request):
     logout(request)
     return redirect("home")
 
+
 def signin(request):
 
     if request.method == "GET":
-        return render(request, "signin.html", {"form":AuthenticationForm})
+        return render(request, "signin.html", {"form": AuthenticationForm})
     else:
-        user = authenticate(request, username=request.POST["username"], password=request.POST["password"])
+        user = authenticate(
+            request,
+            username=request.POST["username"],
+            password=request.POST["password"],
+        )
         if user is None:
-            return render(request, "signin.html", {"form":AuthenticationForm, "error":"Username or Password is incorrect"})
+            return render(
+                request,
+                "signin.html",
+                {
+                    "form": AuthenticationForm,
+                    "error": "Username or Password is incorrect",
+                },
+            )
         else:
             login(request, user)
             return redirect("/projects")
